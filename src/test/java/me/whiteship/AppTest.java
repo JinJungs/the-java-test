cea671c8ae31c1e42067a813de2e20fd4c3f7210
JinJungs <nehciyz@gmail.com>
2024-04-01 22:45:49 +0900

    프로젝트 생성

---
+++ src/test/java/me/whiteship/AppTest.java
@@ -0,0 +1,15 @@
+package me.whiteship;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void create() {
+        App app = new App();
+        assertNotNull(app);
+    }
+
+}
