b636566c8942fa58ae31a86361002225d1aded8f
JinJungs <nehciyz@gmail.com>
2024-04-21 18:09:32 +0900

    [섹션0] junit 마이그레이션
    - org.junit.vintage 를 주석처리하면 Junit4도 사용이 가능하다
    - 단, 기존에 Junit4로 작성된 코드가 있다면 Junit5로 바꾸는 것을 추천한다.
    - Runwith(SpringRunner.class) 는 이미 SpringBootTest에 포함되어 있기 때문에 작성할 필요가 없다.

---
+++ src/test/java/me/whiteship/section0/StudyJUnit4Test.java
@@ -0,0 +1,18 @@
+package me.whiteship.section0;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudyJUnit4Test {
+
+    @Before
+    public void before() {
+        System.out.println("before");
+    }
+
+    @Test
+    public void createTest() {
+        System.out.println("test");
+    }
+}
